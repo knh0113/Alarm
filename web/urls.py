2ece6b179b8c4b67acb487a19c892c01858fb015
JuYeong <happyu9986@gmail.com>
2021-06-25 10:24:34 +0900

    회원가입 구현

--- web/urls.py
+++ web/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login, name="login"),
     path('signup/', views.signup, name="signup"),
     path('alarm/', views.alarm, name="alarm"),
+    path('logout/', views.logout, name="logout"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from .models import User
 from .models import Like
 from .models import Follow
@@ -10,7 +10,30 @@ def main(request):
 
 
 def login(request):
-    return render(request, 'web/login.html')
+    response_data = {}
+
+    if request.method == "GET" :
+        return render(request, 'web/login.html')
+
+    elif request.method == "POST":
+        login_username = request.POST.get('username', None)
+        login_password = request.POST.get('password', None)
+
+
+        if not (login_username and login_password):
+            response_data['error']="아이디와 비밀번호를 모두 입력해주세요."
+        else : 
+            myuser = User.objects.get(username=login_username) 
+            #db에서 꺼내는 명령. Post로 받아온 username으로 , db의 username을 꺼내온다.
+            if check_password(login_password, myuser.password):
+                request.session['user'] = myuser.id 
+                #세션도 딕셔너리 변수 사용과 똑같이 사용하면 된다.
+                #세션 user라는 key에 방금 로그인한 id를 저장한것.
+                return render(request, 'web/main.html')
+            else:
+                response_data['error'] = "비밀번호를 틀렸습니다."
+
+        return render(request, 'web/login.html', response_data)
 
     
 def signup(request):
@@ -18,16 +41,19 @@ def signup(request):
         return render(request, 'web/signup.html')
 
     elif request.method == 'POST':
-        username = request.POST.get['username']
-        password = request.POST.get['password']
-        usercollege = request.POST.get['usercollege']
-        re_password = request.POST.get['re_password']
+        username = request.POST['username']
+        password = request.POST['password']
+        usercollege = request.POST['usercollege']
+        userid = request.POST['userid']
+        re_password = request.POST['re_password']
         res_data = {}
         if not (username and password and re_password) :
             res_data['error'] = "모든 값을 입력해야 합니다."
+            return redirect('signup')
         if password != re_password :
             # return HttpResponse('비밀번호가 다릅니다.')
             res_data['error'] = '비밀번호가 다릅니다.'
+            return redirect('signup')
         else :
             user = User(username=username, password=make_password(password), usercollege=usercollege)
             user.save()
@@ -35,3 +61,7 @@ def signup(request):
 
 def alarm(request):
     return render(request, 'web/alarm.html')
+
+def logout(request):
+    request.session.pop('user')
+    return redirect('/')
